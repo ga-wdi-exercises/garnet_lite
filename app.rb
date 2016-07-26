717130868933a9a9f4c5812ea19515bdb29ad2fe
Andy <andrew.sunglae.kim@gmail.com>
2016-07-26 12:33:14 -0400

    adds full CRUD functionality

--- app.rb
+++ app.rb
@@ -1,7 +1,8 @@
 require 'sinatra'
 require 'sinatra/reloader'
 require 'active_record'
-
+require 'pg'
+require 'pry'
 # Load the file to connect to the DB
 require_relative 'db/connection'
 
@@ -10,5 +11,41 @@ require_relative 'models/instructor'
 require_relative 'models/student'
 
 get '/' do
-  "hello"
+  redirect '/instructors'
+end
+
+get '/instructors' do
+  @instructors = Instructor.all
+  erb :"instructors/index"
+end
+
+get '/instructors/new' do
+  erb :"instructors/new"
+end
+
+get '/instructors/:id' do
+  @instructor = Instructor.find(params[:id])
+  erb :"instructors/show"
+end
+
+post '/instructors' do
+  instructor = Instructor.create(params[:instructor])
+  redirect "/instructors/#{instructor.id}"
+end
+
+get '/instructors/:id/edit' do
+  @instructor = Instructor.find(params[:id])
+  erb :"instructors/edit"
+end
+
+put '/instructors/:id' do
+  instructor = Instructor.find(params[:id])
+  instructor.update(params[:instructor])
+  redirect "/instructors/#{instructor.id}"
+end
+
+delete '/instructors/:id' do
+  instructor = Instructor.find(params[:id])
+  instructor.destroy
+  redirect '/instructors'
 end
